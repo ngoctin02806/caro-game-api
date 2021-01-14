1ded33505b1156cdd5e418a1d0a99524851114ee
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2021-01-14 09:29:34 +0700

    fix: trans to vietnamese

--- src/app/controller/adminStats.controller.js
+++ src/app/controller/adminStats.controller.js
@@ -31,13 +31,13 @@ function startOfWeek(date) {
 
 const numberToDayWeek = number => {
   const data = {
-    1: 'Sunday',
-    2: 'Monday',
-    3: 'Tuesday',
-    4: 'Wednesday',
-    5: 'Thursday',
-    6: 'Friday',
-    7: 'Saturday',
+    1: 'Chủ nhật',
+    2: 'Thứ 2',
+    3: 'Thứ 3',
+    4: 'Thứ 4',
+    5: 'Thứ 5',
+    6: 'Thứ 6',
+    7: 'Thứ 7',
   };
 
   return data[number];
